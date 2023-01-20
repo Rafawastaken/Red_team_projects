4afd90a36387dad9e80151081550f470b8e47958
Rafael Pimenta <jrafapimenta@gmail.com>
2023-01-20 22:52:17 +0000

    navbar, flash messages and add user to db

--- Level_3/39_http_server_phising/phising/backend/routes.py
+++ Level_3/39_http_server_phising/phising/backend/routes.py
@@ -1,6 +1,8 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, flash
 from flask_login import login_required, current_user, logout_user, login_user
 
+
+from phising import app, db
 from .forms import AddUserForm
 from .models import User
 
@@ -13,9 +15,24 @@ backend = Blueprint('backend', __name__)
 def home_backend():
     return render_template('backend/home.html')
 
-@backend.route('/add-user')
+@backend.route('/add-user', methods = ['POST', 'GET'])
 def add_user():
     form = AddUserForm()
+
+    # add user to database
+    if form.validate_on_submit():
+        alias = form.alias.data
+        password = form.password.data
+
+        new_user = User(alias = alias, password = password)
+        db.session.add(new_user)
+        db.session.commit()
+
+        flash(f"User - {alias} - added with success!", "success")
+        return "User adicionado"
+    else:
+        flash("Something went wrong", "danger")
+
     return render_template('backend/register.html', form = form)
 
 @backend.route('/login')
